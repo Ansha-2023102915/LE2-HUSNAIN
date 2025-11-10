7c25b121c5bc356931d5bfebea47deb356caf9a4
Ansha-2023102915 <155720647+Ansha-2023102915@users.noreply.github.com>
2025-11-11 03:45:04 +0800

    Add files via upload

---
+++ BlogDataLibrary/Database/SqlDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Database
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string sql, U parameters, string connectionStringName)
+        {
+            string connStr = _config.GetConnectionString(connectionStringName);
+            using (IDbConnection conn = new SqlConnection(connStr))
+            {
+                var result = await conn.QueryAsync<T>(sql, parameters);
+                return result;
+            }
+        }
+
+        public async Task<int> SaveData<T>(string sql, T parameters, string connectionStringName)
+        {
+            string connStr = _config.GetConnectionString(connectionStringName);
+            using (IDbConnection conn = new SqlConnection(connStr))
+            {
+                var rows = await conn.ExecuteAsync(sql, parameters);
+                return rows;
+            }
+        }
+    }
+}
---
+++ BlogDataLibrary/Models/PostModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Models
+{
+    public class PostModels
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+        public int UserId { get; set; }
+        public DateTime CreatedAt
+        {
+            get; set;
+        }
+    }
+}
